55897d056ba333033275d3738de5a1dec0f71bf4
Azkona05 <135417707+Azkona05@users.noreply.github.com>
2025-03-24 10:21:06 +0100

    cambios

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -2,13 +2,15 @@ package controlador;
 
 import excepciones.LoginException;
 import modelo.Usuario;
+import vista.VMenuPrincipal;
 
 public class Principal {
 
 	private static DaoImplementacion di = new DaoImplementacion();
 	
 	public static void main(String[] args) {
-		
+		VMenuPrincipal vmp = new VMenuPrincipal();
+		vmp.setVisible(true);
 	}
 
 	public static void login(Usuario usuario) throws LoginException{
