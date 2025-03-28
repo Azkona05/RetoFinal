29161a9006d12b41446dd8085cdad9d51163eeff
Azkona05 <135417707+Azkona05@users.noreply.github.com>
2025-03-28 09:54:02 +0100

    commit sprint1

--- src/vista/VMenuPrincipal.java
+++ src/vista/VMenuPrincipal.java
@@ -10,6 +10,7 @@ import java.util.TreeMap;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
