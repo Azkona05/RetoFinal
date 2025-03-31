dc0087bdfa319a807f3e458bc7228dbbb82b8b58
Ander719 <ander.arilla99@gmail.com>
2025-03-31 11:40:49 +0200

    Base Funcional

--- src/test/TestCompeticion.java
+++ src/test/TestCompeticion.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import modelo.Competicion;
 
